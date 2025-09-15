9de0dbd7a2d98fa91a8019db942827c5bd379ec2
Antonio Almir <149828632+AlmirNeto87@users.noreply.github.com>
2025-09-15 00:35:52 -0300

    Implementado Final

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -51,7 +51,7 @@ namespace DesafioProjetoHospedagem.Models
             valor = DiasReservados * Suite.ValorDiaria;
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTADO*
-            if (DiasReservados >=10)
+            if (DiasReservados >10)
             {
                 valor -= (valor * 0.10M);
             }
